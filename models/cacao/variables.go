b5661a0c5580cb1990c1c7914c419d95a406e246
Hidde-Jan Jongsma <hidde-jan.jongsma@tno.nl>
2024-03-04 11:13:06 +0100

    Move variable models to main cacao file

--- models/cacao/variables.go
+++ models/cacao/variables.go
@@ -5,39 +5,36 @@ import (
 	"strings"
 )
 
-type Variable struct {
-	Type        string `bson:"type" json:"type" validate:"required"`
-	Name        string `bson:"name,omitempty" json:"name,omitempty"`
-	Description string `bson:"description,omitempty" json:"description,omitempty"`
-	Value       string `bson:"value,omitempty" json:"value,omitempty"`
-	Constant    bool   `bson:"constant,omitempty" json:"constant,omitempty"`
-	External    bool   `bson:"external,omitempty" json:"external,omitempty"`
-}
-
-type VariableMap map[string]Variable
-
+// Merge two maps of Cacao variables
+//
+// Existing values are not overwritten unless the replacement has been
+// marked as constant.
 func (variableMap VariableMap) Merge(otherMap VariableMap) VariableMap {
 	newMap := make(VariableMap)
-	for k, v := range variableMap {
-		newMap[k] = v
+	for key, value := range variableMap {
+		newMap[key] = value
 	}
 
-	for k, v := range otherMap {
-		if _, ok := variableMap[k]; !ok || v.Constant {
-			newMap[k] = v
+	for key, value := range otherMap {
+		if _, ok := variableMap[key]; !ok || value.Constant {
+			newMap[key] = value
 		}
 	}
 
 	return newMap
 }
 
-// TODO: Find a way to store the Replacer 'inside' the VariableMap
+// Replace variable references in a string
+//
+// Variable substitution is performed according to the CACAO spec,
+// which states `__var__:value` is replaced with the value of `__var__`.
 func (variableMap VariableMap) Replace(s string) string {
 	replacements := make([]string, 0)
-	for k, v := range variableMap {
-		replacementKey := fmt.Sprintf("%s:value", k)
-		replacements = append(replacements, replacementKey, v.Value)
+	for key, value := range variableMap {
+		replacementKey := fmt.Sprintf("%s:value", key)
+		replacements = append(replacements, replacementKey, value.Value)
 	}
-	r := strings.NewReplacer(replacements...)
-	return r.Replace(s)
+	replacer := strings.NewReplacer(replacements...)
+	return replacer.Replace(s)
 }
+
