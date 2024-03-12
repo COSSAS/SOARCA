76f028d8a4e0fa9a43f04028d09d3e017191fbf1
Maarten de Kruijf <maarten.dekruijf@tno.nl>
2024-03-12 20:28:10 +0100

    Added manual fin controller test

---
+++ test/manual/capability/capability_controller_test.go
@@ -0,0 +1,32 @@
+package capability_controller_test
+
+import (
+	"fmt"
+	"soarca/internal/capability/controller"
+	"testing"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func TestConnect(t *testing.T) {
+	// used for manual testing
+
+	// var executionId, _ = uuid.Parse("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
+	// var playbookId = "playbook--d09351a2-a075-40c8-8054-0b7c423db83f"
+	// var stepId = "step--81eff59f-d084-4324-9e0a-59e353dbd28f"
+	// guid := new(guid.Guid)
+	// prot := protocol.FinProtocol{Guid: guid, Topic: protocol.Topic("testing"), Broker: "localhost", Port: 1883}
+
+	options := mqtt.NewClientOptions()
+	options.AddBroker(fmt.Sprintf("mqtt://localhost:1883"))
+	options.SetClientID("soarca")
+	options.SetUsername("public")
+	options.SetPassword("password")
+
+	client := mqtt.NewClient(options)
+
+	finController := controller.New(client)
+	err := finController.Start("localhost", 1883)
+	fmt.Print(err)
+
+}
