8a539436f3e3994acda2e55cffc0f7a78fd6f607
luca.morgese@tno.nl <luca.morgese@tno.nl>
2024-04-04 16:45:30 +0200

    add reporter mock

---
+++ test/unittest/mocks/mock_reporter/mock_reporter.go
@@ -0,0 +1,20 @@
+package mock_reporter
+
+import (
+	"soarca/models/cacao"
+	"soarca/models/execution"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type Mock_Reporter struct {
+	mock.Mock
+}
+
+func (reporter *Mock_Reporter) ReportWorkflow(executionContext execution.Metadata, playbook cacao.Playbook) error {
+	return nil
+}
+
+func (reporter *Mock_Reporter) ReportStep(executionContext execution.Metadata, step cacao.Step, outVars cacao.Variables, err error) error {
+	return nil
+}
