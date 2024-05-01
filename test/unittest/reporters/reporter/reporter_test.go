3f2d36d0c405ed34c0a439bd50e46e0ea0e72a90
luca.morgese@tno.nl <luca.morgese@tno.nl>
2024-05-01 13:51:11 +0200

    add start end reporting functions test for high level reporter

--- test/unittest/reporters/reporter/reporter_test.go
+++ test/unittest/reporters/reporter/reporter_test.go
@@ -100,6 +100,112 @@ func TestReportWorkflowStart(t *testing.T) {
 
 	mock_ds_reporter.On("ReportWorkflowStart", executionId, playbook).Return(nil)
 	reporter.ReportWorkflowStart(executionId, playbook)
+
+	mock_ds_reporter.AssertExpectations(t)
+}
+
+func TestReportWorkflowEnd(t *testing.T) {
+	mock_ds_reporter := mock_reporter.Mock_Downstream_Reporter{}
+	reporter := reporter.New([]ds_reporter.IDownStreamReporter{&mock_ds_reporter})
+
+	expectedCommand := cacao.Command{
+		Type:    "ssh",
+		Command: "ssh ls -la",
+	}
+
+	expectedVariables := cacao.Variable{
+		Type:  "string",
+		Name:  "var1",
+		Value: "testing",
+	}
+
+	step1 := cacao.Step{
+		Type:          "action",
+		ID:            "action--test",
+		Name:          "ssh-tests",
+		StepVariables: cacao.NewVariables(expectedVariables),
+		Commands:      []cacao.Command{expectedCommand},
+		Cases:         map[string]string{},
+		OnCompletion:  "end--test",
+		Agent:         "agent1",
+		Targets:       []string{"target1"},
+	}
+
+	end := cacao.Step{
+		Type: "end",
+		ID:   "end--test",
+		Name: "end step",
+	}
+
+	expectedAuth := cacao.AuthenticationInformation{
+		Name: "user",
+		ID:   "auth1",
+	}
+
+	expectedTarget := cacao.AgentTarget{
+		Name:               "sometarget",
+		AuthInfoIdentifier: "auth1",
+		ID:                 "target1",
+	}
+
+	expectedAgent := cacao.AgentTarget{
+		Type: "soarca",
+		Name: "soarca-ssh",
+	}
+
+	playbook := cacao.Playbook{
+		ID:                            "test",
+		Type:                          "test",
+		Name:                          "ssh-test",
+		WorkflowStart:                 step1.ID,
+		AuthenticationInfoDefinitions: map[string]cacao.AuthenticationInformation{"id": expectedAuth},
+		AgentDefinitions:              map[string]cacao.AgentTarget{"agent1": expectedAgent},
+		TargetDefinitions:             map[string]cacao.AgentTarget{"target1": expectedTarget},
+
+		Workflow: map[string]cacao.Step{step1.ID: step1, end.ID: end},
+	}
+
+	executionId, _ := uuid.Parse("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
+
+	mock_ds_reporter.On("ReportWorkflowEnd", executionId, playbook).Return(nil)
+	reporter.ReportWorkflowEnd(executionId, playbook)
+
+	mock_ds_reporter.AssertExpectations(t)
+}
+
+func TestReportStepStart(t *testing.T) {
+	mock_ds_reporter := mock_reporter.Mock_Downstream_Reporter{}
+	reporter := reporter.New([]ds_reporter.IDownStreamReporter{&mock_ds_reporter})
+
+	expectedCommand := cacao.Command{
+		Type:    "ssh",
+		Command: "ssh ls -la",
+	}
+
+	expectedVariables := cacao.Variable{
+		Type:  "string",
+		Name:  "var1",
+		Value: "testing",
+	}
+
+	step1 := cacao.Step{
+		Type:          "action",
+		ID:            "action--test",
+		Name:          "ssh-tests",
+		StepVariables: cacao.NewVariables(expectedVariables),
+		Commands:      []cacao.Command{expectedCommand},
+		Cases:         map[string]string{},
+		OnCompletion:  "end--test",
+		Agent:         "agent1",
+		Targets:       []string{"target1"},
+	}
+
+	executionId, _ := uuid.Parse("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
+
+	mock_ds_reporter.On("ReportStepStart", executionId, step1, cacao.NewVariables(expectedVariables), nil).Return(nil)
+	reporter.ReportStepStart(executionId, step1, cacao.NewVariables(expectedVariables), nil)
+
+	mock_ds_reporter.AssertExpectations(t)
 }
 
 func TestReportStepEnd(t *testing.T) {
@@ -133,4 +239,6 @@ func TestReportStepEnd(t *testing.T) {
 
 	mock_ds_reporter.On("ReportStepEnd", executionId, step1, cacao.NewVariables(expectedVariables), nil).Return(nil)
 	reporter.ReportStepEnd(executionId, step1, cacao.NewVariables(expectedVariables), nil)
+
+	mock_ds_reporter.AssertExpectations(t)
 }
