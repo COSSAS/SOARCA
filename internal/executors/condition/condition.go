89cb1fed6b02b76e21ecdbd83f0f821c7c9af58b
Maarten de Kruijf <maarten.dekruijf@tno.nl>
2024-05-03 09:26:54 +0200

    Moved to condition

--- internal/executors/condition/condition.go
+++ internal/executors/condition/condition.go
@@ -1,4 +1,4 @@
-package ifcondition
+package condition
 
 import (
 	"errors"
@@ -7,6 +7,7 @@ import (
 	"soarca/logger"
 	"soarca/models/cacao"
 	"soarca/models/execution"
+	"soarca/utils/stix"
 )
 
 var component = reflect.TypeOf(Executor{}).PkgPath()
@@ -24,19 +25,37 @@ func New(capabilities map[string]capability.ICapability) *Executor {
 
 type IExecuter interface {
 	Execute(metadata execution.Metadata,
-		step cacao.Step) (string, error)
+		step cacao.Step) (string, bool, error)
 }
 
 type Executor struct {
 	capabilities map[string]capability.ICapability
 }
 
-func (executor *Executor) Execute(meta execution.Metadata, step cacao.Step) (string, error) {
+func (executor *Executor) Execute(meta execution.Metadata, step cacao.Step) (string, bool, error) {
 
 	if step.Type != cacao.StepTypeIfCondition {
 		err := errors.New("the provided step type is not compatible with this executor")
 		log.Error(err)
-		return step.OnFailure, err
+		return step.OnFailure, false, err
 	}
-	return "", nil
+
+	result, err := stix.Evaluate(step.Condition, step.StepVariables)
+	if err != nil {
+		log.Error(err)
+		return "", false, err
+	}
+
+	if result {
+		if step.OnTrue != "" {
+			log.Trace("")
+			return step.OnTrue, true, nil
+		}
+	} else {
+		if step.OnFalse != "" {
+			return step.OnFalse, true, nil
+		}
+	}
+
+	return step.OnCompletion, false, nil
 }
