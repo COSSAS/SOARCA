0d164037dc3c07c3434dc9d632a01ee005909add
luca.morgese@tno.nl <luca.morgese@tno.nl>
2024-05-14 13:33:27 +0200

    remove unused fcns from cache mock

--- test/unittest/mocks/mock_cache/mock_cache.go
+++ test/unittest/mocks/mock_cache/mock_cache.go
@@ -1,8 +1,6 @@
 package mock_cache
 
 import (
-	"soarca/models/cacao"
-
 	cache_model "soarca/models/cache"
 
 	"github.com/google/uuid"
@@ -11,24 +9,6 @@ import (
 
 type Mock_Cache struct {
 	mock.Mock
-}
-
-func (reporter *Mock_Cache) ReportWorkflowStart(executionId uuid.UUID, playbook cacao.Playbook) error {
-	args := reporter.Called(executionId, playbook)
-	return args.Error(0)
-}
-func (reporter *Mock_Cache) ReportWorkflowEnd(executionId uuid.UUID, playbook cacao.Playbook, workflowError error) error {
-	args := reporter.Called(executionId, playbook, workflowError)
-	return args.Error(0)
-}
-
-func (reporter *Mock_Cache) ReportStepStart(executionId uuid.UUID, step cacao.Step, stepResults cacao.Variables) error {
-	args := reporter.Called(executionId, step, stepResults)
-	return args.Error(0)
-}
-func (reporter *Mock_Cache) ReportStepEnd(executionId uuid.UUID, step cacao.Step, stepResults cacao.Variables, stepError error) error {
-	args := reporter.Called(executionId, step, stepResults, stepError)
-	return args.Error(0)
 }
 
 func (reporter *Mock_Cache) GetExecutions() ([]cache_model.ExecutionEntry, error) {
