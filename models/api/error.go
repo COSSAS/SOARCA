f38d4703adf4d44a4830017201b3ed8412f0abd2
Maarten de Kruijf <maarten.dekruijf@tno.nl>
2024-05-21 08:00:32 +0200

    Added api error model and example

---
+++ models/api/error.go
@@ -0,0 +1,8 @@
+package api
+
+type Error struct {
+	Status         int    `json:"status" validate:"required" example:"400"`
+	Message        string `json:"message" validate:"required" example:"missing argument in call"`
+	OriginalCall   string `json:"original-call" validate:"required" example:"/example/route"`
+	DownstreamCall string `json:"downstream-call" validate:"omitempty" example:"{"some" : "json"}" `
+}
