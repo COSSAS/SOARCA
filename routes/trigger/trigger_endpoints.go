a624c52b32eacb07370e1d7725656dcb0d32ca9a
Maarten de Kruijf <maarten.dekruijf@tno.nl>
2024-05-21 16:45:56 +0200

    Fixed failure routes

--- routes/trigger/trigger_endpoints.go
+++ routes/trigger/trigger_endpoints.go
@@ -5,15 +5,16 @@ import (
 )
 
 // trigger
+//
 //	@Summary	trigger a playbook by supplying a cacao playbook payload
 //	@Schemes
 //	@Description	trigger playbook
 //	@Tags			trigger
 //	@Accept			json
 //	@Produce		json
-//	@Param			playbook	body	cacao.Playbook	true	"execute playbook by payload"
-//	@Success		200			"{"execution_id":"uuid","payload":"playbook--uuid"}"
-//	@error			400
+//	@Param			playbook	body		cacao.Playbook	true	"execute playbook by payload"
+//	@Success		200			{object}	api.Execution
+//	@failure		400			{object}	api.Error
 //	@Router			/trigger/playbook [POST]
 func Routes(route *gin.Engine, trigger *TriggerApi) {
 	group := route.Group("/trigger")
