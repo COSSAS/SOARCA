d2ff989a32e0ab9be3646cc264442fc697643709
Maarten de Kruijf <maarten.dekruijf@tno.nl>
2024-05-21 10:05:13 +0200

    Removed space form formatting line

--- models/api/error.go
+++ models/api/error.go
@@ -4,5 +4,5 @@ type Error struct {
 	Status         int    `json:"status" validate:"required" example:"400"`
 	Message        string `json:"message" validate:"required" example:"missing argument in call"`
 	OriginalCall   string `json:"original-call" validate:"required" example:"/example/route"`
-	DownstreamCall string `json:"downstream-call" validate:"omitempty" example:"{"some" : "json"}" `
+	DownstreamCall string `json:"downstream-call" validate:"omitempty" example:"{"some" : "json"}"`
 }
