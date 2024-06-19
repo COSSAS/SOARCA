6a3bd189d170c00690efe30dadd115759ba517bb
Maarten de Kruijf <maarten.dekruijf@tno.nl>
2024-06-19 11:11:06 +0200

    Added status test

--- test/integration/api/api_test.go
+++ test/integration/api/api_test.go
@@ -2,9 +2,11 @@ package api_test
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"net/http"
 	"soarca/internal/controller"
+	"soarca/models/api"
 	"testing"
 	"time"
 
@@ -120,5 +122,43 @@ func TestPingPong(t *testing.T) {
 	assert.Equal(t, nil, err)
 
 	assert.Equal(t, "pong", string(body))
+
+}
+
+func TestStatus(t *testing.T) {
+	// Start SOARCA in separate threat
+	t.Setenv("PORT", "8083")
+	go initializeSoarca(t)
+
+	// Wait for the server to be online
+	time.Sleep(400 * time.Millisecond)
+
+	client := http.Client{}
+	buffer := bytes.NewBufferString("")
+	request, err := http.NewRequest("GET", "http://localhost:8083/status", buffer)
+	if err != nil {
+		t.Fail()
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	assert.Equal(t, nil, err)
+
+	status := api.Status{}
+	err = json.Unmarshal(body, &status)
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, "production", status.Mode)
+	assert.NotEmpty(t, status.Mode)
+	assert.NotEmpty(t, status.Runtime)
+	assert.NotEmpty(t, status.Time)
+	t.Log(status)
 
 }
