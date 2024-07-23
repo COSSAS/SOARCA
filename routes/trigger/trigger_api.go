b931a56f85c0756b8c92ba6cf86370cf87dbc9bd
luca.morgese@tno.nl <luca.morgese@tno.nl>
2024-07-23 13:09:59 +0200

    fixing lint check on execute return values

--- routes/trigger/trigger_api.go
+++ routes/trigger/trigger_api.go
@@ -60,7 +60,9 @@ func (trigger *TriggerApi) Execute(context *gin.Context) {
 		return
 	}
 
-	go decomposer.Execute(*playbook, trigger.Executionsch)
+	go func() {
+		_, _ = decomposer.Execute(*playbook, trigger.Executionsch)
+	}()
 
 	// Hard coding the timer to return execution id
 	timer := time.NewTimer(time.Duration(3) * time.Second)
