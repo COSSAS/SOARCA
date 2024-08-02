3f77655916bd47b6528dd4902a53b4f39199aeef
Maarten de Kruijf <maarten.dekruijf@tno.nl>
2024-08-02 15:43:11 +0200

    removed trigger description from main route

--- routes/trigger/trigger_endpoints.go
+++ routes/trigger/trigger_endpoints.go
@@ -4,21 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// trigger
-//
-//	@Summary	trigger a playbook by supplying a cacao playbook payload
-//	@Schemes
-//	@Description	trigger playbook
-//	@Tags			trigger
-//	@Accept			json
-//	@Produce		json
-//	@Param			playbook	body		cacao.Playbook	true	"execute playbook by payload"
-//	@Success		200			{object}	api.Execution
-//	@failure		400			{object}	api.Error
-//	@Router			/trigger/playbook [POST]
 func Routes(route *gin.Engine, trigger *TriggerApi) {
 	group := route.Group("/trigger")
 	{
 		group.POST("/playbook", trigger.Execute)
+		group.POST("/playbook/:id", trigger.ExecuteById)
 	}
 }
