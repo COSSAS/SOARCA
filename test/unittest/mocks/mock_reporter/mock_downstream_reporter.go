096a3fef3ec8fe4208d0fdedb47857695b9f6749
luca.morgese@tno.nl <luca.morgese@tno.nl>
2024-08-08 10:32:55 +0200

    reporting sent downstream via go routine and adjust testing accordingly

--- test/unittest/mocks/mock_reporter/mock_downstream_reporter.go
+++ test/unittest/mocks/mock_reporter/mock_downstream_reporter.go
@@ -2,6 +2,7 @@ package mock_reporter
 
 import (
 	"soarca/models/cacao"
+	"sync"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/mock"
@@ -9,22 +10,27 @@ import (
 
 type Mock_Downstream_Reporter struct {
 	mock.Mock
+	Wg *sync.WaitGroup
 }
 
-func (reporter *Mock_Downstream_Reporter) ReportWorkflowStart(executionId uuid.UUID, playbook cacao.Playbook) error {
-	args := reporter.Called(executionId, playbook)
+func (ds_reporter *Mock_Downstream_Reporter) ReportWorkflowStart(executionId uuid.UUID, playbook cacao.Playbook) error {
+	defer ds_reporter.Wg.Done()
+	args := ds_reporter.Called(executionId, playbook)
 	return args.Error(0)
 }
-func (reporter *Mock_Downstream_Reporter) ReportWorkflowEnd(executionId uuid.UUID, playbook cacao.Playbook, workflowError error) error {
-	args := reporter.Called(executionId, playbook, workflowError)
+func (ds_reporter *Mock_Downstream_Reporter) ReportWorkflowEnd(executionId uuid.UUID, playbook cacao.Playbook, workflowError error) error {
+	defer ds_reporter.Wg.Done()
+	args := ds_reporter.Called(executionId, playbook, workflowError)
 	return args.Error(0)
 }
 
-func (reporter *Mock_Downstream_Reporter) ReportStepStart(executionId uuid.UUID, step cacao.Step, stepResults cacao.Variables) error {
-	args := reporter.Called(executionId, step, stepResults)
+func (ds_reporter *Mock_Downstream_Reporter) ReportStepStart(executionId uuid.UUID, step cacao.Step, stepResults cacao.Variables) error {
+	defer ds_reporter.Wg.Done()
+	args := ds_reporter.Called(executionId, step, stepResults)
 	return args.Error(0)
 }
-func (reporter *Mock_Downstream_Reporter) ReportStepEnd(executionId uuid.UUID, step cacao.Step, stepResults cacao.Variables, stepError error) error {
-	args := reporter.Called(executionId, step, stepResults, stepError)
+func (ds_reporter *Mock_Downstream_Reporter) ReportStepEnd(executionId uuid.UUID, step cacao.Step, stepResults cacao.Variables, stepError error) error {
+	defer ds_reporter.Wg.Done()
+	args := ds_reporter.Called(executionId, step, stepResults, stepError)
 	return args.Error(0)
 }
