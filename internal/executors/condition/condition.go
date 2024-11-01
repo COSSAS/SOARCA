0600dbc3e9053292cacb462060114ed047523056
Maarten de Kruijf <16100232+MaartendeKruijf@users.noreply.github.com>
2024-11-01 17:13:48 +0100

    Updated variables for reporting in condition executor

--- internal/executors/condition/condition.go
+++ internal/executors/condition/condition.go
@@ -46,22 +46,14 @@ func (executor *Executor) Execute(meta execution.Metadata, step cacao.Step, vari
 
 	executor.reporter.ReportStepStart(meta.ExecutionId, step, variables, executor.time.Now())
 
-	var reportErr error
+	var err error
 	defer func() {
-		executor.reporter.ReportStepEnd(meta.ExecutionId, step, variables, reportErr, executor.time.Now())
+		executor.reporter.ReportStepEnd(meta.ExecutionId, step, variables, err, executor.time.Now())
 	}()
 
 	result, err := executor.comparison.Evaluate(step.Condition, variables)
-
-	// executor.reporter.ReportStepEnd(meta.ExecutionId,
-	// 	step,
-	// 	variables,
-	// 	err,
-	// 	executor.time.Now())
-
 	if err != nil {
 		log.Error(err)
-		reportErr = err
 		return "", false, err
 	}
 
