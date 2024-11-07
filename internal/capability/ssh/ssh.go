cd4bfa2188ef72aa6c0758399e664649e8b85860
Maarten de Kruijf <16100232+MaartendeKruijf@users.noreply.github.com>
2024-11-07 15:52:19 +0100

    refactor ssh capability (#246)

--- internal/capability/ssh/ssh.go
+++ internal/capability/ssh/ssh.go
@@ -37,60 +37,36 @@ func (sshCapability *SshCapability) Execute(metadata execution.Metadata,
 	authentication cacao.AuthenticationInformation,
 	target cacao.AgentTarget,
 	variables cacao.Variables) (cacao.Variables, error) {
+
 	log.Trace(metadata.ExecutionId)
+	return execute(command, authentication, target)
+}
 
-	host := CombinePortAndAddress(target.Address, target.Port)
+func execute(command cacao.Command,
+	authentication cacao.AuthenticationInformation,
+	target cacao.AgentTarget) (cacao.Variables, error) {
 
-	errAuth := CheckSshAuthenticationInfo(authentication)
+	err := CheckSshAuthenticationInfo(authentication)
 
-	if errAuth != nil {
-		log.Error(errAuth)
-		return cacao.NewVariables(), errAuth
-	} else {
-		log.Trace(host)
-	}
-
-	var config ssh.ClientConfig
-
-	if authentication.Type == "user-auth" {
-		config = ssh.ClientConfig{
-			User: authentication.Username,
-			Auth: []ssh.AuthMethod{
-				ssh.Password(authentication.Password),
-			},
-			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-			Timeout:         time.Duration(time.Second * 20),
-		}
-	} else if authentication.Type == "private-key" {
-		signer, errKey := ssh.ParsePrivateKey([]byte(authentication.PrivateKey))
-		if errKey != nil || authentication.Password == "" {
-			log.Error("no valid authentication information: ", errKey)
-			return cacao.NewVariables(), errKey
-		}
-		config = ssh.ClientConfig{
-			User: authentication.Username,
-			Auth: []ssh.AuthMethod{
-				ssh.PublicKeys(signer),
-			},
-			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-			Timeout:         time.Duration(time.Second * 20),
-		}
-
-	}
-
-	var conn *ssh.Client
-
-	conn, err := ssh.Dial("tcp", host, &config)
 	if err != nil {
 		log.Error(err)
 		return cacao.NewVariables(), err
 	}
-	var session *ssh.Session
-	session, err = conn.NewSession()
+	config, err := getConfig(authentication)
 	if err != nil {
-		log.Error(err)
 		return cacao.NewVariables(), err
 	}
+	session, client, err := getSession(config, target)
+	defer client.Close()
+	if err != nil {
+		return cacao.NewVariables(), err
+	}
+
+	return executeCommand(session, command)
+}
+
+func executeCommand(session *ssh.Session,
+	command cacao.Command) (cacao.Variables, error) {
 
 	response, err := session.Output(StripSshPrepend(command.Command))
 	defer session.Close()
@@ -106,6 +82,50 @@ func (sshCapability *SshCapability) Execute(metadata execution.Metadata,
 	return results, err
 }
 
+func getConfig(authentication cacao.AuthenticationInformation) (ssh.ClientConfig, error) {
+	config := ssh.ClientConfig{User: authentication.Username,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         time.Duration(time.Second * 20)}
+
+	switch authentication.Type {
+	case "user-auth":
+		config.Auth = []ssh.AuthMethod{
+			ssh.Password(authentication.Password)}
+		return config, nil
+
+	case "private-key":
+		signer, err := ssh.ParsePrivateKey([]byte(authentication.PrivateKey))
+		if err != nil || authentication.Password == "" {
+			log.Error("no valid authentication information: ", err)
+			return config, err
+		}
+		config.Auth = []ssh.AuthMethod{
+			ssh.PublicKeys(signer)}
+		return config, nil
+
+	default:
+		return config, errors.New("non supported authentication type")
+	}
+
+}
+
+func getSession(config ssh.ClientConfig, target cacao.AgentTarget) (*ssh.Session, *ssh.Client, error) {
+	host := CombinePortAndAddress(target.Address, target.Port)
+	client, err := ssh.Dial("tcp", host, &config)
+	if err != nil {
+		log.Error(err)
+		return nil, nil, err
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		log.Error(err)
+		client.Close()
+		return nil, nil, err
+	}
+	return session, client, err
+}
+
 func CombinePortAndAddress(addresses map[cacao.NetAddressType][]string, port string) string {
 	if port == "" {
 		port = "22"
@@ -117,7 +137,6 @@ func CombinePortAndAddress(addresses map[cacao.NetAddressType][]string, port str
 		}
 	}
 	return ""
-
 }
 
 func StripSshPrepend(command string) string {
@@ -136,13 +155,12 @@ func CheckSshAuthenticationInfo(authentication cacao.AuthenticationInformation) 
 		if strings.TrimSpace(authentication.Password) == "" {
 			return errors.New("password is empty")
 		}
-		return nil
 	} else if authentication.Type == "private-key" {
 		if strings.TrimSpace(authentication.PrivateKey) == "" {
 			return errors.New("private key is not set")
 		}
-		return nil
 	} else {
 		return errors.New("non supported authentication type")
 	}
+	return nil
 }
