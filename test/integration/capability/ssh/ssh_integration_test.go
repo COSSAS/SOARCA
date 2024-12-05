aa1001120716c0f26ed3ef1692029c8514cc2b71
Maarten de Kruijf <16100232+MaartendeKruijf@users.noreply.github.com>
2024-12-05 16:27:23 +0100

    Added regression test for segfault #276

--- test/integration/capability/ssh/ssh_integration_test.go
+++ test/integration/capability/ssh/ssh_integration_test.go
@@ -8,6 +8,7 @@ import (
 	"soarca/pkg/models/execution"
 	"testing"
 
+	"github.com/go-playground/assert/v2"
 	"github.com/google/uuid"
 )
 
@@ -54,6 +55,51 @@ func TestSshConnection(t *testing.T) {
 		fmt.Println(err)
 		t.Fail()
 	}
+
+	fmt.Println(results)
+
+}
+
+func TestSshConnectionToNonExistingServer(t *testing.T) {
+	sshCapability := new(ssh.SshCapability)
+
+	expectedCommand := cacao.Command{
+		Type:    "ssh",
+		Command: "ls -la",
+	}
+
+	expectedAuthenticationInformation := cacao.AuthenticationInformation{
+		ID:       "some-authid-1",
+		Type:     "user-auth",
+		Username: "sshtest",
+		Password: "pdKY77qNxpI5MAizirtjCVOcm0KFKIs"}
+
+	expectedTarget := cacao.AgentTarget{
+		Type:    "ssh",
+		Address: map[cacao.NetAddressType][]string{"ipv4": {"10.10.10.10"}},
+		// Port:               "22",
+		AuthInfoIdentifier: "some-authid-1",
+	}
+
+	expectedVariables := cacao.Variable{
+		Type:  "string",
+		Name:  "var1",
+		Value: "testing",
+	}
+
+	var executionId, _ = uuid.Parse("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
+	var playbookId = "playbook--d09351a2-a075-40c8-8054-0b7c423db83f"
+	var stepId = "step--81eff59f-d084-4324-9e0a-59e353dbd28f"
+	var metadata = execution.Metadata{ExecutionId: executionId, PlaybookId: playbookId, StepId: stepId}
+	data := capability.Context{
+		Command:        expectedCommand,
+		Target:         expectedTarget,
+		Authentication: expectedAuthenticationInformation,
+		Variables:      cacao.NewVariables(expectedVariables),
+	}
+	results, err := sshCapability.Execute(metadata,
+		data)
+	assert.NotEqual(t, err, nil)
 
 	fmt.Println(results)
 
