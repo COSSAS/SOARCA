ad3c1b40006f8165213df5b9159b8ab937b028d8
RabbITCybErSeC <jpkonijn@protonmail.ch>
2025-04-01 17:27:25 +0200

    added SOARCA_URI to env

--- ui/soarca-gui/src/pages/LoginPage.tsx
+++ ui/soarca-gui/src/pages/LoginPage.tsx
@@ -12,8 +12,6 @@ const LoginPage: React.FC = () => {
   const handleLoginSuccess = () => {
     console.log("Login successful!");
     setErrors([]);
-    // TODO: Redirect user or update global auth state here
-    // Example: navigate('/dashboard'); or authContext.login(userData);
   };
 
   const handleLoginError = (receivedErrors: Error[]) => {
@@ -23,17 +21,15 @@ const LoginPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-slate-800 flex flex-col justify-center sm:py-12 font-sans">
       <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-        <div className="flex justify-center mb-5">
-          <a href={homeLink}>
-            {/* Ensure the image path is correctly handled by your build process (e.g., placing it in the public folder) */}
-            <img src={soarcaLogoUrlPath} alt="SOARCA Logo" className="w-30 h-auto" />
-          </a>
-        </div>
         <div className="bg-white dark:bg-slate-900 shadow w-full rounded-lg divide-y divide-gray-200 dark:divide-slate-700">
           <div className="px-5 pt-7">
+            <div className="flex justify-center mb-5">
+              <a href={homeLink}>
+                <img src={soarcaLogoUrlPath} alt="SOARCA Logo" className="w-30 h-auto" />
+              </a>
+            </div>
             <AuthErrorDisplay errors={errors} />
           </div>
-          {/* Assuming LoginForm, AuthErrorDisplay, LoginFooter are correctly implemented and imported */}
           <LoginForm onSubmitSuccess={handleLoginSuccess} onSubmitError={handleLoginError} />
           <LoginFooter />
         </div>
--- ui/soarca-gui/vite.config.ts
+++ ui/soarca-gui/vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv, type UserConfig, type ConfigEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    define: {
+      'process.env.SOARCA_URI': JSON.stringify(env.SOARCA_URI),
+    },
+    server: {
+      port: 5173,
+      host: true,
+      watch: {
+        usePolling: true,
+      },
+    },
+    esbuild: {
+      target: 'esnext',
+    }
+  };
+});
