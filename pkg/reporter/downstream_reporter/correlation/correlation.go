69aa25782026b354e4d25d7d060c052d4f9c7f19
Maarten de Kruijf <16100232+MaartendeKruijf@users.noreply.github.com>
2025-07-28 14:13:00 +0200

    Added correlation

---
+++ pkg/reporter/downstream_reporter/correlation/correlation.go
@@ -0,0 +1,74 @@
+package correlation
+
+import (
+	"reflect"
+	"soarca/internal/logger"
+	"soarca/pkg/models/cacao"
+	"soarca/pkg/models/execution"
+	"soarca/pkg/reporter/downstream_reporter/correlation/incident"
+	"soarca/pkg/utils/guid"
+
+	"github.com/google/uuid"
+)
+
+type Empty struct{}
+
+var component = reflect.TypeOf(Empty{}).PkgPath()
+var log *logger.Log
+
+func init() {
+	log = logger.Logger(component, logger.Info, "", logger.Json)
+}
+
+type ICorrelation interface {
+	Correlate(execution.Metadata, cacao.Variables) (string, error)
+}
+
+type ICorrelationIntegration interface {
+}
+
+type Cases map[uuid.UUID]incident.ICase
+
+type Correlation struct {
+	cases Cases
+	guid  guid.IGuid
+}
+
+func New(guid guid.IGuid) Correlation {
+	return Correlation{cases: Cases{}, guid: guid}
+}
+
+func (correlation *Correlation) Correlate(meta execution.Metadata, variables cacao.Variables) (string, error) {
+	for _, item := range correlation.cases {
+		for _, variable := range variables {
+			if part, err := item.CheckObservableInCase(variable); err != nil {
+				return "", err
+			} else if part {
+				log.Debug(part)
+				correlation.addExecutionToCase(item, meta, variable)
+				return item.GetId().String(), nil
+			}
+		}
+	}
+	log.Info("not in case creating a new case")
+	id := correlation.createCase(meta, variables)
+	return id.String(), nil
+}
+
+// func (correlation *Correlation) addEddxecutionToCase(meta execution.Metadata, variables cacao.Variables) error {
+// 	return nil
+// }
+
+// func (Correlation *Correlation) searchForCase() {
+
+// }
+
+func (correlation *Correlation) addExecutionToCase(item incident.ICase, meta execution.Metadata, variable cacao.Variable) {
+	correlation.cases[item.GetId()].AddIfNotInCase(meta, variable)
+}
+
+func (correlation *Correlation) createCase(meta execution.Metadata, variables cacao.Variables) uuid.UUID {
+	thisCase := incident.New(correlation.guid.NewV7(), meta, variables)
+	correlation.cases[thisCase.GetId()] = thisCase
+	return thisCase.GetId()
+}
