67af9b8a768f23697c43a07337f3a09d8c2befd3
Maarten de Kruijf <16100232+MaartendeKruijf@users.noreply.github.com>
2025-07-28 12:06:21 +0200

    Added observable

---
+++ pkg/reporter/downstream_reporter/correlation/observable/observable_test.go
@@ -0,0 +1,38 @@
+package observable
+
+import (
+	"soarca/pkg/models/cacao"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/google/uuid"
+)
+
+func TestExecutions(t *testing.T) {
+
+	id, err := uuid.NewV7()
+	assert.Equal(t, err, nil)
+	obs := Observable{Type: SourceAddressIpv4, Name: "__SRC_IP__", Value: "10.0.0.1", Executions: Executions{id: Initial}}
+
+	id2, err := uuid.NewV7()
+	assert.Equal(t, err, nil)
+	obs.AddExecutionToObservable(id2, SameObservable)
+
+	assert.Equal(t, obs.Executions[id], Initial)
+	assert.Equal(t, obs.Executions[id2], SameObservable)
+
+}
+
+func TestCreate(t *testing.T) {
+
+	var1 := cacao.Variable{Type: cacao.VariableTypeIpv4Address, Name: "__SRC_IP__", Description: "source ip", Value: "10.0.0.1"}
+
+	obs, err := CreateObservable(var1)
+	assert.Equal(t, err, nil)
+
+	id2, err := uuid.NewV7()
+	assert.Equal(t, err, nil)
+	obs.AddExecutionToObservable(id2, SameObservable)
+	assert.Equal(t, obs.Executions[id2], SameObservable)
+
+}
