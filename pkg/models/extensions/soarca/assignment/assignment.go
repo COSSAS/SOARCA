df2a47f3fe8db776f4a8d83c85a65973efa8f898
Maarten de Kruijf <16100232+MaartendeKruijf@users.noreply.github.com>
2026-06-26 15:34:57 +0200

    Added decode to assignment

--- pkg/models/extensions/soarca/assignment/assignment.go
+++ pkg/models/extensions/soarca/assignment/assignment.go
@@ -1,5 +1,9 @@
 package assignment
 
+import "encoding/json"
+
+const AssignmentExtensionType = "soarca-assignment"
+
 type Expression struct {
 	Type       string `json:"type"`
 	Expression string `json:"expression"`
@@ -10,4 +14,19 @@ type Assignment struct {
 	StepResult string     `json:"step-result"`
 	Variable   string     `json:"variable"`
 	Expression Expression `json:"expression"`
+}
+
+func DecodeAssignment(raw interface{}) (Assignment, bool) {
+	encoded, err := json.Marshal(raw)
+	if err != nil {
+		return Assignment{}, false
+	}
+	var model Assignment
+	if err := json.Unmarshal(encoded, &model); err != nil {
+		return Assignment{}, false
+	}
+	if model.Type != AssignmentExtensionType {
+		return Assignment{}, false
+	}
+	return model, true
 }
